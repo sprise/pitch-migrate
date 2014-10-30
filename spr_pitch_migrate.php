3ed69c8e419a5de4e6462025355033236024dc6c
lg <lg@sprisemedia.com>
2014-10-30 12:01:06 -0700

    It works, just need to stop stripping the newlines/p tags

--- spr_pitch_migrate.php
+++ spr_pitch_migrate.php
@@ -36,15 +36,20 @@ class SPR_Pitch_Migrate {
 	var $links = array();
 	var $posts = array();
 	var $nl;
+	var $msg = '';
 	
 	public function __construct(){
 		// Default setup
 		$this->config['debug'] = 'n';
 		$this->config['title'] = 'Pitch Engine Import';
+		$this->config['slug'] = 'pitch-migrate.php';
 		$this->config['post-wrap'] = '.pitch-post';
 		$this->config['post-type'] = 'post';
-		$this->config['src'] = 'src.html'; 
+		$this->config['src'] = __DIR__.'/src.html'; 
+		$this->config['limit'] = 0;
+		$this->config['sort'] = 'ASC';
 		$this->posts = array();
+		$this->msg = '';
 		
 		// Create settings page
 		add_action( 'admin_menu', array($this, 'admin_pane') );
@@ -53,24 +58,37 @@ class SPR_Pitch_Migrate {
 	
 	
 	
-	/* WPize */
+	/* WP Admin Page */
 	
 	public function admin_pane() {		
-		add_plugins_page($this->config['title'], $this->config['title'], 'edit_theme_options', 'pitch-migrate.php', array($this, 'admin_pane_render') );
+		add_plugins_page(
+			$this->config['title'], 
+			$this->config['title'], 
+			'edit_posts', 
+			$this->config['slug'], 
+			array($this, 'admin_pane_render') 
+		);
 	}
 
 	public function admin_pane_render(){	
-		if(isset($_POST['submit']) && $_POST['task'] == 'do-import') $this->do_wp_import( (int) $_POST['limit'] );
-		//elseif(isset($_POST['submit']) && $_POST['task'] == 'check-links') $this->do_wp_import( (int) $_POST['limit'] );
+		// Page Submitted
+		if(isset($_POST['submit']) && $_POST['task'] == 'do-import') $this->do_wp_import();
+	
 		$emstyle = 'style="font-size: .9em;"';
 		
 		$html = '<h1 style="padding: 40px 0 20px 0;">Pitch Engine Content Import</h1>';
-		$html .= '<p>Enter in some shortcodes below and they (and their output) will be removed from your pages and posts. Remove from the list to reinstate the shortcode.</p>';
+		if($this->msg != '') $html .= '<p style="color:red; font-weight: bold;">'.$this->msg.'</p>';
 		
-		$html .= '<form method="post" action="'.site_url().'/wp-admin/admin.php?page=pitch-migrate.php">';
+		$html .= '<p>Customize options and click the button below to import from <strong>src.html</strong> (upload this to wp-content/plugins/spr-pitch-migrate).</p>';
+		
+		$html .= '<form method="post" action="'.site_url().'/wp-admin/admin.php?page='.$this->config['slug'].'">';
 		$html .= '<input type="hidden" name="task" value="do-import" />';		
 		
-		$html .= '<p><input class="button-primary button-large" type="submit" name="submit" value="Submit" /></p>';
+		$html .= '<p>Number of posts: 	<input type="text" name="limit" value="1" /></p>';
+		$html .= '<p>Post type: 		<input type="text" name="post-type" value="post" /></p>';
+		$html .= '<p>Order: <select name="sort"><option value="ASC">Oldest First</option><option value="DESC">Newest First</option></select></p>';
+		$html .= '<p>Debug: <select name="debug"><option value="n">Off</option><option value="y">On</option></select></p>';
+		$html .= '<p><input class="button-primary button-large" type="submit" name="submit" value="Import Content" /></p>';
 		$html .= '</form>';
 		
 		echo $html;
@@ -108,30 +126,49 @@ class SPR_Pitch_Migrate {
 	
 	/* Lets do it */
 	
-	public function do_wp_import($limit = 0){
+	public function do_wp_import(){
+		require 'vendor/autoload.php';
 		$this->dom = new simple_html_dom();
-		$limit = (int) $limit;
+		
+		// How many posts? Post type? Debug?
+		$this->_setup_options();
 				
 		// Squeeze the links from the feed html
 		$this->_get_links();
 		
 		// Get post data from each linked page
-		$this->_get_posts($limit);
+		$this->_get_posts($this->config['limit']);
 				
 		// Do the import
-		$this->_import_posts();
-				
-		return $this->posts;
+		$qty = $this->_import_posts();
+		
+		$this->msg = $qty.' new posts were imported.';
+	}
+	
+	protected function _setup_options(){
+		if(isset($_POST['limit']))	 $this->config['limit'] = (int) $_POST['limit']; 	 	
+		if(isset($_POST['sort'])) 	 $this->config['sort'] = $_POST['sort']; 	
+		
+		if(isset($_POST['debug']))	 $this->set_debug($this->sanitize($_POST['debug'])); 	
+		if(isset($_POST['post-type'])) $this->set_posttype($this->sanitize($_POST['post-type']));
 	}
 	
 	protected function _get_links(){
-		if(empty($this->config['src']) || !file_exists($this->config['src'])) die('Invalid source file.');
+		if(empty($this->config['src']) || !file_exists($this->config['src'])) 
+			die('Invalid source file: '.$this->config['src']);
 		
 		$html = file_get_html($this->config['src']);
 		
 		// Add post links to $this->links array
-		foreach($html->find($this->config['post-wrap'].' a') as $row) 
-			if($row->href != '#' && !in_array($row->href,$this->links)) $this->links[] = $row->href;
+		foreach($html->find($this->config['post-wrap'].' a') as $row){ 
+			if($row->href == '#' || in_array($row->href,$this->links)) continue; // no dupes
+			
+			// Latest first
+			if($this->config['sort'] == 'DESC') $this->links[] = $row->href;
+		
+			// Earliest posts first
+			else array_unshift($this->links, $row->href);
+		}
 		
 		$this->showme($this->links,'Links');
 	}
@@ -166,19 +203,24 @@ class SPR_Pitch_Migrate {
 		}
 	}
 	
-	protected function _import_posts(){
-		$temp = $this->posts;
-		rsort($temp); // Earliest first
+	protected function _import_posts(){	
 		$newid = 0;
+		$img_base = site_url().'/uploads/'.date('Y').'/'.date('m');
 		
 		$count = 0;
-		foreach($temp as $row) {
+		foreach($this->posts as $row) {
+			$body = sanitize_text_field($row['content']);
+			
 			// Correct image urls
 			if(!empty($row['imgs'])){
-				$img_base = site_url();
-				
+				foreach($row['imgs'] as $img){
+					$filename = substr($img,strrpos($img,'/'));
+					$new = $img_base.$filename;
+					
+					$body = str_replace($img, $new, $body);
+				}
 			}
-			
+
 			// Save as post			
 			$postdata = array(
 				'post_name'      => sanitize_title($row['title']),	
@@ -186,16 +228,48 @@ class SPR_Pitch_Migrate {
 				'post_status'    => 'publish', 					
 				'post_type'      => $this->config['post-type'], 
 				'post_author'    => 1,			 				
-				'post_date'      => date('Y-m-d H:i:s', mktime())
+				'post_date'      => date('Y-m-d H:i:s', mktime()),
+				'post_content'	 => $body
 			);  
-			//$newid = wp_insert_post($postdata);
+			
+			//$this->showme($postdata); die();
+			$newid = wp_insert_post($postdata);
 			
 			// Save images if app
 			if(!empty($row['imgs'])){
 				$tid = 0;
 				
-				foreach($row['imgs'] as $img) {
-					$img_id = wp_insert_attachment( $args, $img, $newid );
+				for($i=0;$i<count($row['imgs']);$i++) {
+					if(empty($row['imgs'][$i])) continue;
+					
+					$tmp = download_url($row['imgs'][$i]);
+					
+					// Set variables for storage
+					// fix file filename for query strings
+					preg_match('/[^\?]+\.(jpg|JPG|jpe|JPE|jpeg|JPEG|gif|GIF|png|PNG)/', $row['imgs'][$i], $matches);
+					$file_array['name'] = basename($matches[0]);
+					$file_array['tmp_name'] = $tmp;
+
+					// If error storing temporarily, unlink
+					if ( is_wp_error( $tmp ) ) {
+						@unlink($file_array['tmp_name']);
+						$file_array['tmp_name'] = '';
+					}
+					// do the validation and storage stuff
+					$thumbid = media_handle_sideload( $file_array, $newid );
+					
+					// If error storing permanently, unlink
+					if ( is_wp_error($thumbid) ) {
+						@unlink($file_array['tmp_name']);
+						//return $thumbid;
+					}
+
+					$args = array();
+					
+					//$img_id = wp_insert_attachment( $args, $row['imgs'][$i], $newid );
+					
+					// Save a Featured image
+					if($i == 0) set_post_thumbnail( $newid, $thumbid );
 				}
 			}
 			
@@ -203,7 +277,9 @@ class SPR_Pitch_Migrate {
 			$count++;
 		}
 		
-		$this->showme($count,'Rows Imported');		
+		$this->showme($count,'Rows Imported');
+		
+		return $count;		
 	}
 
 
@@ -218,6 +294,11 @@ class SPR_Pitch_Migrate {
 		echo '</pre>';
 	}
 	
+	protected function sanitize($var){
+		return trim(filter_var($var, FILTER_SANITIZE_STRING, 'FILTER_ENCODE_HIGH'));
+	}
+	
 }
 
+if(!isset($spr_pitch) || !is_object($spr_pitch)) $spr_pitch = new SPR_Pitch_Migrate();  
 } // class exists check
